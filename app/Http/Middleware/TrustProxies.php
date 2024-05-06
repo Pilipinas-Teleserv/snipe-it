dd3c70f959d240141863bbc426a7be19aeb08327
JustineHemor <hemor.justine14@gmail.com>
2024-05-06 13:40:57 +0800

    Trusts divine-axis as proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.40.5.8',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
